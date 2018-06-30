575a29ec2330f6865b9177dc11e7c43a0260e746
郑欢 <zhenghuan@sinafenqi.com>
2018-06-30 14:51:02 +0800

    初始化，接入xposed hook

---
+++ app/src/main/java/com/pince/hook/Tutorial.kt
@@ -0,0 +1,11 @@
+package com.pince.hook
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Tutorial: IXposedHookLoadPackage{
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
